64095eb8a69c3431d785823dbd8d3ce3b4cf41c8
wayuto <4e07910@gmail.com>
2025-12-11 23:27:55 +0800

    feat: simple std

--- gos-std/target/debug/libgos_std.d
+++
@@ -1,1 +0,0 @@
-/home/w/Gos/gos-std/target/debug/libgos_std.a: /home/w/Gos/gos-std/src/convert.rs /home/w/Gos/gos-std/src/gosio.rs /home/w/Gos/gos-std/src/lib.rs /home/w/Gos/gos-std/src/string.rs
--- gos-std/target/release/libgos_std.d
+++
@@ -1,1 +0,0 @@
-/home/w/Gos/gos-std/target/release/libgos_std.a: /home/w/Gos/gos-std/src/convert.rs /home/w/Gos/gos-std/src/gosio.rs /home/w/Gos/gos-std/src/lib.rs /home/w/Gos/gos-std/src/string.rs
